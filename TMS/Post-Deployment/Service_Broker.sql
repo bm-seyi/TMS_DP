c7a329233463e34975b527cbf3f37dec737971bf
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2025-01-06 21:15:24 +0000

    0.0.6

--- TMS/Post-Deployment/Service_Broker.sql
+++ TMS/Post-Deployment/Service_Broker.sql
@@ -1,2 +1,5 @@
 -- Write your own SQL object definition here, and it'll be included in your package.
+ALTER DATABASE TMS SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 ALTER DATABASE TMS SET ENABLE_BROKER;
+-- After enabling the Service Broker, switch it back to multi-user mode:
+ALTER DATABASE TMS SET MULTI_USER;
--- TMS/Script.PostDeployment.sql
+++ TMS/Script.PostDeployment.sql
@@ -1,2 +1,2 @@
 -- This file contains SQL statements that will be executed after the build script.
-:r ./Post-Deployment/Service_Broker.sql
+:r .\Post-Deployment\Service_Broker.sql
